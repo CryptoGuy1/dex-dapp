2c193a9be11d420f694fce638980a23dd9abda1a
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-03-04 13:33:49 +0100

    Added transfer function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,8 +7,16 @@ contract Token {
   uint256 public decimals = 18;
   uint256 public totalSupply;
 
+  mapping(address => uint256) public balanceOf;
+
   constructor() {
     totalSupply = 1000000 * (10**decimals);
+    balanceOf[msg.sender] = totalSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    balanceOf[_to] = balanceOf[_to].add(_value);
   }
 
 }
